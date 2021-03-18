3a7d9ae1db6c60cefb98c5d97e345e490d2979eb
Kajalben <kajalsavani191@gmail.com>
2021-03-18 14:46:49 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const db = require('./database/client');
+
+const restaurantRouter = require('./routes/restaurantRouter');
+
+const app = express();
+ 
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/restaurant', restaurantRouter);
+
+module.exports = app;
---
+++ database/client.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGO_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database connection established successfully");
+  })
+  .catch((e) => console.log(e.message));
+
+const client = mongoose.connection;
+client.on("error", (e) => {
+  console.log(e.message);
+});
+
+module.exports = client;
