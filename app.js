4612bac75da7478a0418f87e67ef606c9b7d9c75
Selma456 <74097172+sm456456@users.noreply.github.com>
2021-03-18 19:06:30 +0100

    add tag router

--- app.js
+++ app.js
@@ -8,9 +8,10 @@ const db = require('./database/client');
 
 const restaurantRouter = require('./routes/restaurantRouter');
 const cityRouter = require('./routes/cityRouter');
+const tagRouter = require('./routes/tagRouter');
 
 const app = express();
- 
+
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,5 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/restaurant', restaurantRouter);
 app.use('/city', cityRouter);
+app.use('/tag', tagRouter);
 
 module.exports = app;
--- routes/restaurantRouter.js
+++ routes/restaurantRouter.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const {createRestaurent, getRestarants, getOneRestarant} = require('../controllers/restaurantController');
 
-/* GET home page. */
 router.get('/', getRestarants);
 router.get('/:id', getOneRestarant); 
 router.post('/', createRestaurent)
